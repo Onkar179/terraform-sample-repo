01b81c9b78ac36d4e198c961f8e677eaf352e91e
Onkar Naik <onkar.naik@forescout.com>
2024-06-06 13:43:54 +0530

    terraform-code-scanning

--- spanner-instance/main.tf
+++ spanner-instance/main.tf
@@ -4,12 +4,6 @@ variable "enable_soar" {
   description = "description"
 }
 
-provider "aws" {
-  region     = "us-west-2"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
-}
-
 module "cloud_spanner" {
   source = "../modules/spanner-instance"
   for_each = var.enable_soar ? var.instances : {}
@@ -21,5 +15,39 @@ module "cloud_spanner" {
   instance_size = each.value.instance_size
   region = each.value.instance_config
   instance_labels = each.value.instance_labels
-  password = "123"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "my-vpc-network"
+  project = "onkar-17-cloud"
+}
+
+# Create a firewall rule to allow SSH access
+resource "google_compute_firewall" "allow-ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+  project = "onkar-17-cloud"
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "example-instance"
+  machine_type = "f1-micro"
+  project  = "onkar-17-cloud"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+  zone         = "us-central1-b"
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
 }
--- spanner-instance/terraform.tfvars
+++ spanner-instance/terraform.tfvars
@@ -11,22 +11,22 @@ test-instance = {
     "env" = "demo"
   }
   instance_iam = [
-    # "user:onkar.naik@forescout.com=>roles/spanner.databaseAdmin"
+    "user:onkar.naik@forescout.com=>roles/spanner.databaseAdmin"
   ]
 }
-# test-instance-2 = {
-#   project_id  = "onkar-17-cloud"
-#   instance_display_name = "Spanner demo"
-#   create_instance = true
-#   instance_size = {
-#     processing_units = 100
-#   }
-#   instance_config = "regional-us-central1"
-#   instance_labels = {
-#     "env" = "demo"
-#   }
-#   instance_iam = [
-#     # "user:onkar.naik@forescout.com=>roles/spanner.databaseAdmin"
-#   ]
-# }
+ test-instance-2 = {
+   project_id  = "onkar-17-cloud"
+   instance_display_name = "Spanner prod"
+   create_instance = true
+   instance_size = {
+     processing_units = 200
+   }
+   instance_config = "regional-us-central1"
+   instance_labels = {
+     "env" = "prod"
+   }
+  instance_iam = [
+     "user:dibanyshu.rai@forescout.com=>roles/spanner.databaseAdmin"
+  ]
+ }
 }
