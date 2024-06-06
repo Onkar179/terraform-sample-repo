4d734baab96325955980d808294c5a2ad4868c24
Onkar Naik <66739607+Onkar179@users.noreply.github.com>
2024-06-06 21:19:18 +0530

    Update main.tf

--- spanner-instance/main.tf
+++ spanner-instance/main.tf
@@ -16,38 +16,3 @@ module "cloud_spanner" {
   region = each.value.instance_config
   instance_labels = each.value.instance_labels
 }
-
-resource "google_compute_network" "vpc_network" {
-  name = "my-vpc-network"
-  project = "onkar-17-cloud"
-}
-
-# Create a firewall rule to allow SSH access
-resource "google_compute_firewall" "allow-ssh" {
-  name    = "allow-ssh"
-  network = google_compute_network.vpc_network.name
-  project = "onkar-17-cloud"
-  allow {
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-}
-
-resource "google_compute_instance" "vm_instance" {
-  name         = "example-instance"
-  machine_type = "f1-micro"
-  project  = "onkar-17-cloud"
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-  zone         = "us-central1-b"
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
-}
